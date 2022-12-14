1bdd087dd6ecd0cc14789ef33bc2f367eb412d4a
Jonathan Huergo <jh2000@hotmail.com.ar>
2022-12-14 08:09:34 -0300

    Initial commit

---
+++ components/Layout.js
@@ -0,0 +1,27 @@
+import Head from 'next/head'
+
+const Layout = ({children}) => {
+    return ( 
+        <>
+        <Head>
+            <meta charset="utf-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="theme-color" content="#000000" />
+            <meta
+            name="description"
+            content="My personal portfolio"
+            />
+            {/* <meta
+            property="og:image"
+            content=""
+            /> */}
+            <title>Jonathan Huergo - Front End Developer</title>
+        </Head>
+        <main>
+            {children}
+        </main>
+        </>
+    );
+}
+ 
+export default Layout;
