03b994db1dbd9cdc7e10389ad12513510347d4e9
Jonathan Huergo <jh2000@hotmail.com.ar>
2022-12-14 08:28:44 -0300

    Add About component

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
+import About from './about'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
+    <>
     <section>
       <h1>Soy Jonathan Huergo,</h1>
       <h2>desarrollador Front End y estudiante de Ingeniería Informática</h2>
     </section>
+    <About/>
+    </>
   )
 }
