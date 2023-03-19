81de29c7a2512bfee1ecc3424c1f4738da1ed8f9
Jonathan Huergo <jh2000@hotmail.com.ar>
2023-03-19 17:40:58 -0300

    Move header text up

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,8 +5,10 @@ export default function Home() {
   return (
     <>
     <section id="home" className='header'>
-      <h1>Soy Jonathan Huergo,</h1>
-      <h2>desarrollador Front End y estudiante de Ingeniería Informática</h2>
+      <div className='header-text'>
+        <h1>Soy Jonathan Huergo,</h1>
+        <h2>desarrollador Front End y estudiante de Ingeniería Informática</h2>
+      </div>
       <a href="#contact"><button type="button" className="contact-button"><b>CONTACTAME</b></button></a>
     </section>
     <Info/>
