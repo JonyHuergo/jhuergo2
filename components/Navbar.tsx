3772c1e9e1c530665c6d0519627e6d6e22b29bdd
Jonathan Huergo <jh2000@hotmail.com.ar>
2023-03-19 17:43:05 -0300

    Add projects link to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,8 +7,9 @@ const Navbar = () => {
                 <a href="#home" className="logo">JH</a>
             </div>
             <div className="nav-links">
-                <a href="#about">Acerca de mi</a>
                 <a href="#contact">Contacto</a>
+                <a href="#about">Acerca de mi</a>
+                <a href="#projects">Proyectos</a>
             </div>
             {/* <a href="javascript:void(0);" className="navbar-icon" onclick="myFunction()">
                 <i className="fa fa-bars"></i>
