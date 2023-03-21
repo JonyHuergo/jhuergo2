0b176c3b25a641875c0065a9628e228de13da4a3
Jonathan Huergo <jh2000@hotmail.com.ar>
2023-03-21 11:50:13 -0300

    Style projects section title

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -4,7 +4,7 @@ import styles from "../styles/Projects.module.css"
 const Projects = () => {
     return (
         <section id="projects" className={styles.projects}>
-            <h2>PROJECTS</h2>
+            <h2>PROYECTOS</h2>
             <div className={styles.projectsContainer}>
                 <ProjectCard name={"History-Blog"} img={"historyBlog.png"} pageLink={"https://history-blog-gamma.vercel.app/"} technologies={["HTML", "CSS", "JAVASCRIPT", "REACT", "NEXT", "GRAPHQL", "TAILWIND"]}/>
                 <ProjectCard name={"Comercio Conectado"} img={"comercioConectado.png"} pageLink={"https://comercio-conectado.vercel.app/"} technologies={["HTML", "CSS", "JAVASCRIPT", "REACT", "NEXT", "GRAPHQL"]}/>
