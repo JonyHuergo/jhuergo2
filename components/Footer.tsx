409425072557e1bbb7eb4e4c3ca40a3dfb2520b3
Jonathan Huergo <jh2000@hotmail.com.ar>
2023-06-07 19:13:35 -0300

    Remove or comment unused sections

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,9 +3,6 @@ const Footer = () => {
         <footer id="sticky-footer">
             <div>
                 <small>Copyright &copy; Jonathan Huergo</small>
-                <small>
-                    <p>Made with <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">React</a></p>
-                </small>
             </div>
         </footer>
     );
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
                 <a href="#home" className={styles.logo}>JH</a>
             </div>
             <div className={styles.navbarLinks}>
-                <a href="#contact">Contacto</a>
+                {/* <a href="#contact">Contacto</a> */}
                 <a href="#about">Acerca de mi</a>
                 <a href="#projects">Proyectos</a>
             </div>
