0a85b7560f63a19f749d7835cbe743ca61905b63
Jonathan Huergo <jh2000@hotmail.com.ar>
2023-06-07 20:55:37 -0300

    Cahnge charset to charSet

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -6,7 +6,7 @@ const Layout = ({children}) => {
     return ( 
         <>
         <Head>
-            <meta charset="utf-8" />
+            <meta charSet="utf-8" />
             <meta name="viewport" content="width=device-width, initial-scale=1" />
             <meta name="theme-color" content="#000000" />
             <meta
