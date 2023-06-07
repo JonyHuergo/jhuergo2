af21f0aae898e7fb2dc65b59eeaf6ab5e01404b3
Jonathan Huergo <jh2000@hotmail.com.ar>
2023-06-07 19:28:13 -0300

    Change about me text

--- components/About.tsx
+++ components/About.tsx
@@ -9,11 +9,10 @@ const About = () => {
                     <img className={styles.photo} src="me.jpg" alt="photo" />
                 </div>
                 <div className={styles.aboutTextContainer}>
-                    <h3>Titulo</h3>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-                        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                        Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
-                        Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                    <p>Capacitado en el diseño e implementación de paginas web. Actualmente 
+                    profesionalizándose en Ingeniería Informática en la Universidad de Buenos Aires. Capaz 
+                    de adaptarse a las necesidades de cada proyecto, trabajar independientemente o en 
+                    equipo, y de aprender nuevas tecnologías si es requerido.</p>
                 </div>
             </div>
         </section>
